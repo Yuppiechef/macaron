1f99cd5fc3b3abcd081bd4d3832e88d552539386
Deon Moolman <cmdrdats@gmail.com>
2012-08-14 10:03:05 +0200

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject com.yuppiechef/macaron "1.0.0-SNAPSHOT"
+  :description "A declarative entity abstraction library for clojure."
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/tools.logging "0.2.3"]
+                 [org.clojure/java.jdbc "0.1.0"]])
---
+++ test/lib/test/core.clj
@@ -0,0 +1,6 @@
+(ns lib.test.core
+  (:use [lib.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
